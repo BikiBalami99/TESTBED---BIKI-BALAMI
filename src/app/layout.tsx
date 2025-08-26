0c49cd0ff338e57050cbd6832b04af66bbfec08a
Biki Balami <bikibalami99@gmail.com>
2025-08-26 21:40:39 +0900

    Enhance layout with MonitorStyle component and CSS adjustments
    - Add MonitorStyle component to wrap main content for improved display
    - Update layout.module.css to set max-height for body and adjust main styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./reset.css";
 import "./globals.css";
 import styles from "./layout.module.css";
 import NavBar from "@/features/shared/navbar/NavBar";
+import MonitorStyle from "@/features/shared/MonitorStyle/MonitorStyle";
 
 export default function RootLayout({
 	children,
@@ -12,7 +13,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={styles.body}>
 				<NavBar />
-				<main className={styles.main}>{children}</main>
+				<main className={styles.main}>
+					<MonitorStyle>{children}</MonitorStyle>
+				</main>
 			</body>
 		</html>
 	);
